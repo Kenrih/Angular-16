cdd8e841d23e17350ae506bb8e4a015345795a58
Kenrih <kenrih.korol@gmail.com>
2024-06-04 10:35:51 +0300

    ..

--- Angular16../creditcardadmin/src/app/creditcards/creditcards.component.ts
+++ Angular16../creditcardadmin/src/app/creditcards/creditcards.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CreditCard } from '../models/credit-card';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatPaginator} from '@angular/material/paginator';
+import { MatSelect } from '@angular/material/select';
+import { MatSort } from '@angular/material/sort';
+import { CreditcardsService } from '../services/creditcards.service';
+
 
 @Component({
   selector: 'app-creditcards',
@@ -6,5 +14,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./creditcards.component.scss']
 })
 export class CreditcardsComponent {
+
+  creditcard: CreditCard[] = [];
+
+  constructor(private creditcardsService: CreditcardsService) {
+    this.creditcardsService.getCreditCards().subscribe((data:CreditCard[]) => {
+      this.creditcard = data;
+
+      this.dataSource = new MatTableDataSource(this.creditcard);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
+
+  dataSource = new MatTableDataSource(this.creditcard);
+  
+
+  displayColumns = ["select", "id", "name", "description", "bankName", "maxCredit", "interestRate", "active", "recommendedScore",]
+
+
+  selection = new SelectionModel(true, []);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  selectHandler(row: CreditCard){
+    this.selection.toggle(row as never);
+  }
 
 }
